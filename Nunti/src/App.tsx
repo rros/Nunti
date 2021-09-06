3e6088b91c570a1cde6ddd11210c6717c140c8d1
Richard Klapáč <richardklapac@tutanota.com>
2021-09-06 18:49:55 +0200

    setup project

---
+++ Nunti/src/App.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {
+    Text
+} from 'react-native';
+
+class App extends Component {
+    render() {
+        return (
+            <Text>I have crippling deppression!</Text>
+        );
+    }
+}
+
+export default App;
---
+++ Nunti/src/Styles.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+import { DarkTheme, DefaultTheme } from 'react-native-paper';
+
+// customise default dark and white themes later
+/*export const Dark = {
+    ...DarkTheme,
+    colors: {
+        ...DarkTheme.colors,
+        accentName: "aqua",
+        primary: "#3498db",
+        accent: '#3498db',
+    }
+};
+
+export const Light = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        accentName: "aqua",
+        primary: "#3498db",
+        accent: '#3498db',
+    }
+};*/
+
+// custom css here
+export default Styles = StyleSheet.create ({
+
+});
+
