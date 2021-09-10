6da3bf76aa3344fae9f090b09cf990902690006f
Ondřej Foltýn <6360355-ondrejfoltyn@users.noreply.gitlab.com>
2021-09-10 11:35:20 +0200

    add Backend placeholder class

--- Nunti/src/App.tsx
+++ Nunti/src/App.tsx
@@ -21,6 +21,8 @@ import {
 
 import { WebView } from 'react-native-webview';
 
+import Backend from './Backend';
+
 // TODO UI: loading animation for refresh
 // TODO UI: slide to rate and remove
 // TODO UI: navigation (settings and saved)
@@ -29,11 +31,11 @@ import { WebView } from 'react-native-webview';
 class App extends Component {
     // catch back button // if webview open, then close it and resume app else end
     backAction = () => {
-        if(this.state.reading == true){
+        if(this.state.reading == true) {
             this.setState({ reading: false });
         } else if(this.state.detailsVisible == true){
             this.setState({ detailsVisible: false });
-        } else{
+        } else {
             BackHandler.exitApp();
         }
         return true;
@@ -47,7 +49,7 @@ class App extends Component {
         this.backHandler.remove();
     }
 
-    constructor(){
+    constructor() {
         super();
 
         this.readMore = this.readMore.bind(this);
@@ -60,72 +62,54 @@ class App extends Component {
             detailsVisible: false,
             currentArticle: 0,
             reading: false,
-            refreshing: false, // change this to true when waiting for new articles
-            articles: [ // modify this state to update list of articles // placeholder articles
-                {
-                    id: 0,
-                    title: "Kauza kancléře Mynáře míří k nejvyššímu žalobci Střížovi. Ve hře je vrácení dotace na penzion",
-                    description: "Dotační úřad ROP Střední Morava našel cestu, jak ještě získat zpět šestimilionovou dotaci na penzion v Osvětimanech od firmy Clever Management hradního kancléře Vratislava Mynáře. Vedoucí odboru veřejné kontroly Dana Koplíková serveru iROZHLAS.cz potvrdila, že úřad předal Nejvyššímu státnímu zastupitelství návrh, aby kvůli vrácení dotace podal správní žalobu k soudu.",
-                    cover: "https://www.irozhlas.cz/sites/default/files/styles/zpravy_fotogalerie_medium/public/uploader/profimedia-030438780_170821-154405_kno.jpg?itok=i1w6k_n8",
-                    url: "https://www.irozhlas.cz/zpravy-domov/vratislav-mynar-dotace-rop-stredni-morava-osvetimany-clever-management_2109071907_zpo"
-                },
-                {
-                    id: 1,
-                    title: "Policisté nemohli střelbě v Bělehradské ulici předejít, postupovali správně, uvedla generální inspekce",
-                    description: "Útoku, při kterém koncem června zemřela pracovnice úřadu práce v Praze 2, nebylo možné předejít, uvedla na svém webu Generální inspekce bezpečnostních sborů (GIBS). Postup policie byl podle ní správný a v souladu s předpisy. Ze zastřelení zaměstnankyně úřadu práce je obviněn šestašedesátiletý muž. Podle policistů krátce předtím poleptal kyselinou ženu v Odoleně Vodě a nastražil výbušné zařízení, které zranilo policistu.",
-                    cover: "https://www.irozhlas.cz/sites/default/files/styles/zpravy_fotogalerie_medium/public/uploader/profimedia-061859281_210630-142545_bar.jpg?itok=EUOjsK1J",
-                    url: "https://www.irozhlas.cz/zpravy-domov/policie-postup-strelba-belehradska-urednice-gibs-inspekce_2109071724_tzr"
-                },
-                {
-                    id: 2,
-                    title: "Hamáček vysvětloval na policii plánovanou cestu do Moskvy. K obsahu výpovědi se odmítl vyjádřit",
-                    description: "Vicepremiér a ministr vnitra Jan Hamáček (ČSSD) v úterý podával policistům vysvětlení k okolnostem své neuskutečněné cesty do Moskvy, kterou překazilo odhalení v kauze výbuchů ve Vrběticích. Podle informací České televize policisté dorazili za Hamáčkem dopoledne na ministerstvo vnitra. Pro média se ministr po rozhovoru s vyšetřovateli nevyjadřoval.",
-                    cover: "https://www.irozhlas.cz/sites/default/files/styles/zpravy_fotogalerie_medium/public/uploader/rv0_6117_210628-170605_vlf.jpg?itok=nOyfuGJE",
-                    url: "https://www.irozhlas.cz/zpravy-domov/jan-hamacek-cesta-do-moskvy-vrbetice-vysetrovani_2109071849_onz"
-                },
-            ]
+            refreshing: true, // change this to true when waiting for new articles
+            articles: [{id:0,title: "",description: "",cover:"about:blank",url:"about:blank"}]
         }
         
+        Backend.LoadNewArticles().then( (arts) => {
+            this.setState({articles: arts})
+            this.setState({refreshing: false})
+        });
         // load user saved theme here
         // end splash screen here somewhere
         // begin loading articles
     }
 
-    private async readMore(){
+    private async readMore() {
         this.setState({ reading: true });
     }
     
-    private async viewDetails(){
+    private async viewDetails() {
         this.setState({ detailsVisible: true });
     }
     
-    private async hideDetails(){
+    private async hideDetails() {
         this.setState({ detailsVisible: false });
     }
 
-    private async refresh(){
+    private async refresh() {
         await this.setState({ refreshing: true });
-        // refresh articles here
+        this.setState({ articles: await Backend.LoadNewArticles() });
         await this.setState({ refreshing: false });
     }
 
-    private async saveArticle(){
+    private async saveArticle() {
         // save article to favourites
         console.log("saving to favourites");
     }
 
-    private async shareArticle(){
+    private async shareArticle() {
         await Share.share({
             message: this.state.articles[this.state.currentArticle].url,
         });
     }
 
     render() {
-        if(this.state.reading){
+        if(this.state.reading) {
             return(
                 <WebView source={{ uri: this.state.articles[this.state.currentArticle].url }}/>
             );
-        } else{
+        } else {
             return (
                 <PaperProvider theme={Dark}>
                     <SafeAreaView style={{ flex: 1, backgroundColor: Dark.colors.background }}>
