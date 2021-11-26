32c6b18f6429476ecc90cedfc6d5c3dcabc9785c
Ondřej Foltýn <6360355-ondrejfoltyn@users.noreply.gitlab.com>
2021-11-26 12:50:02 +0100

    add couple of new feeds

--- Nunti/src/DefaultTopics.ts
+++ Nunti/src/DefaultTopics.ts
@@ -6,11 +6,9 @@ export default class DefaultTopics {
             new Feed("https://www.irozhlas.cz/rss/irozhlas"),
             new Feed("https://novinky.cz/rss"),
             new Feed("https://www.aktualne.cz/rss"),
-            new Feed("https://www.root.cz/rss/clanky/"),
             new Feed("https://ct24.ceskatelevize.cz/rss"),
             new Feed("https://www.seznamzpravy.cz/rss"),
-            new Feed("https://www.cnews.cz/rss"),
-            new Feed("https://servis.lidovky.cz/rss.aspx"),
+            new Feed("https://rss.msn.com/cs-cz"),
         ],
         "technology": [
             new Feed("https://rss.nytimes.com/services/xml/rss/nyt/Technology.xml"),
@@ -18,6 +16,21 @@ export default class DefaultTopics {
         ],
         "worldPolitics": [
             new Feed("https://www.reutersagency.com/feed/?post_type=reuters-best"),
+            new Feed("http://rss.cnn.com/rss/edition.rss"),
+            new Feed("https://rss.msn.com/en-GB"),
+        ],
+        "weather": [
+            new Feed("https://rss.accuweather.com/rss/mt-blog-rss.asp?blog=headlines"),
+        ],
+        "economy": [
+            new Feed("https://www.cnbc.com/id/20910258/device/rss/rss.html"),
+            new Feed("https://www.ft.com/global-economy?format=rss"),
+        ],
+        "tabloids": [
+            new Feed("https://www.thesun.co.uk/rss"),
+        ],
+        "travel": [
+            new Feed("https://www.cnbc.com/id/10000739/device/rss/rss.html"),
         ],
     }
 }
