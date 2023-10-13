ebfc2284821635479cf4866f2d98f15608da3582
Ondřej Foltýn <6360355-ondrejfoltyn@users.noreply.gitlab.com>
2023-10-13 15:28:54 +0200

    be: refactor background

--- Nunti/src/Backend/Background.ts
+++ Nunti/src/Backend/Background.ts
@@ -21,7 +21,7 @@ export class Background {
             log.info(`App is not in background (state = ${AppState.currentState}), exiting background task.`);
             return;
         }
-        
+
         // lock mechanism
         if (this.BackgroundLock) {
             log.warn('Another background task already running, exiting now..');
@@ -29,105 +29,116 @@ export class Background {
         }
         this.BackgroundLock = true;
         log.info('BackgroundLock locked.');
-        
+
         try {
             await BackendAPI.Init();
             if (UserSettings.Instance.DisableBackgroundTasks) {
                 log.info('DisableBackgroundTasks enabled, exiting...');
                 return;
             }
-            if (!UserSettings.Instance.EnableBackgroundSync && !UserSettings.Instance.EnableNotifications) {
-                log.info('BackgroundSync and notifications disabled.');
-            } else {
-                try {
-                    if (UserSettings.Instance.EnableBackgroundSync) {
-                        log.debug('BackgroundSync is enabled, checking cache...');
-                        const cache = await Storage.GetArticleCache();
-                        const cacheAgeMinutes = (Date.now() - parseInt(cache.timestamp.toString())) / 60000;
-                        if (cacheAgeMinutes >= UserSettings.Instance.ArticleCacheTime * 0.75) {
-                            log.info('Cache will expire soon, invalidating cache to force re-sync...');
-                            cache.timestamp = 0;
-                            await Storage.FSStore.setItem('cache',JSON.stringify(cache));
-                        }
-                    }
-                    const arts = await BackendAPI.GetArticles('feed');
-                    if (UserSettings.Instance.EnableNotifications) {
-                        const notifcache = await Storage.StorageGet('notifications-cache');
-                        const lastNotificationBeforeMins = (Date.now() - parseInt(notifcache.timestamp.toString())) / 60000;
-                        if (lastNotificationBeforeMins >= UserSettings.Instance.NewArticlesNotificationPeriod) {
-                            notifcache.timestamp = Date.now();
-                            let art: Article | null = null;
-                            for (let i = 0; i < arts.length; i++) {
-                                if (notifcache.seen_urls.indexOf(arts[i].url) < 0) {
-                                    art = arts[i];
-                                    notifcache.seen_urls.push(art.url);
-                                    notifcache.seen_urls.splice(0, notifcache.seen_urls.length - 20); //keep only last 20
-                                    break;
-                                }
-                            }
-                            if (art == null)
-                                log.context('Notifications').warn('No available article to show.');
-                            else {
-                                if(!await BackendAPI.SendNotification(art.title, 'new_articles'))
-                                    throw new Error('Failed to send notification.');
-                            }
-                            await Storage.StorageSave('notifications-cache', notifcache);
-                        } else
-                            log.info(`Will not show notification, time remaining: ${UserSettings.Instance.NewArticlesNotificationPeriod - lastNotificationBeforeMins} mins.`);
-                    } else
-                        log.info('Notifications disabled.');
-                } catch (err) {
-                    log.error(`Exception on backgroundTask, id:${taskId}, error:`, err);
-                }
-            }
-            if (UserSettings.Instance.EnableAutomaticBackups) {
-                const remainingTime = Date.now() - UserSettings.Instance.LastBackupTimestamp - (UserSettings.Instance.AutomaticBackupPeriod * 60 * 60 * 1000);
-                if (remainingTime >= 0) {
-                    const logB = log.context('AutoBackup');
-                    logB.info('Creating auto-backup now..');
-                    const dir = JSON.parse(UserSettings.Instance.AutomaticBackupDir);
-                    if ((await ScopedStorage.getPersistedUriPermissions()).indexOf(dir.url) >= 0) {
-                        logB.error(`Access to ${dir.url} is not in persisted permissions list.`);
-                    } else {
-                        logB.debug(`Auto-backup dir: ${UserSettings.Instance.AutomaticBackupDir}`);
-                        const backupStr = await Backup.CreateBackup();
-                        try {
-                            const files = await ScopedStorage.listFiles(dir.uri);
-                            files.sort((a, b) => {
-                                if (a.name == 'NuntiBackup-latest.json')
-                                    return 1;
-                                else if (b.name == 'NuntiBackup-latest.json')
-                                    return -1;
-                                return a.uri < b.uri ? 1 : -1;
-                            });
-                            for (let i = 0; i < files.length; i++) {
-                                const f = files[i];
-                                if (f.name == 'NuntiBackup-latest.json')
-                                    await ScopedStorage.rename(f.uri, 'NuntiBackup-1.json');
-                                else if (f.name == 'NuntiBackup-1.json')
-                                    await ScopedStorage.rename(f.uri, 'NuntiBackup-2.json');
-                                else if (f.name == 'NuntiBackup-2.json')
-                                    await ScopedStorage.rename(f.uri, 'NuntiBackup-3.json');
-                                else if (f.name == 'NuntiBackup-3.json')
-                                    await ScopedStorage.deleteFile(f.uri);
-                            }
-                            
-                            logB.debug('writing now');
-                            if (await ScopedStorage.writeFile(dir.uri, backupStr, 'NuntiBackup-latest.json', 'application/json', 'utf8') == null) {
-                                throw new Error('Write to NuntiBackup-latest.json failed.');
-                            }
-                        } catch (err) {
-                            logB.error('AutoBackup failed!', err);
-                        }
-                    }
-                    UserSettings.Instance.LastBackupTimestamp = Date.now();
-                    await UserSettings.Save();
-                } else
-                    log.info(`Remaining time until next auto-backup: ${-(remainingTime / (60*60*1000)).toFixed(2)} hrs.`);
-            }
+
+            const arts = await this.TryDoBackgroundSyncAsync();
+            await this.TryDoNotificationAsync(arts);
+            await this.TryDoAutoBackupAsync();
+        } catch (err) {
+            log.error(`Exception on backgroundTask, id:${taskId}, error:`, err);
         } finally {
             log.info('Unlocking BackgroundLock now.');
             this.BackgroundLock = false;
+        }
+    }
+
+    /** Attempts to perform background sync according to user settings, returns fresh articles */
+    private static async TryDoBackgroundSyncAsync(): Promise<Article[]> {
+        if (UserSettings.Instance.EnableBackgroundSync) {
+            const log = Background.log.context("BackgroundSync");
+            log.debug('BackgroundSync is enabled, checking cache...');
+            const cache = await Storage.GetArticleCache();
+            const cacheAgeMinutes = (Date.now() - parseInt(cache.timestamp.toString())) / 60000;
+            if (cacheAgeMinutes >= UserSettings.Instance.ArticleCacheTime * 0.75) {
+                log.info('Cache will expire soon, invalidating cache to force re-sync...');
+                cache.timestamp = 0;
+                await Storage.FSStore.setItem('cache', JSON.stringify(cache));
+            }
+        }
+        return await BackendAPI.GetArticles('feed');
+    }
+
+    private static async TryDoNotificationAsync(arts: Article[]): Promise<void> {
+        const log = Background.log.context("Notifications");
+        if (UserSettings.Instance.EnableNotifications) {
+            const notifcache = await Storage.StorageGet('notifications-cache');
+            const lastNotificationBeforeMins = (Date.now() - parseInt(notifcache.timestamp.toString())) / 60000;
+            if (lastNotificationBeforeMins >= UserSettings.Instance.NewArticlesNotificationPeriod) {
+                notifcache.timestamp = Date.now();
+                let art: Article | null = null;
+                for (let i = 0; i < arts.length; i++) {
+                    if (notifcache.seen_urls.indexOf(arts[i].url) < 0) {
+                        art = arts[i];
+                        notifcache.seen_urls.push(art.url);
+                        notifcache.seen_urls.splice(0, notifcache.seen_urls.length - 20); //keep only last 20
+                        break;
+                    }
+                }
+                if (art == null)
+                    log.context('Notifications').warn('No available article to show.');
+                else {
+                    if (!await BackendAPI.SendNotification(art.title, 'new_articles'))
+                        throw new Error('Failed to send notification.');
+                }
+                await Storage.StorageSave('notifications-cache', notifcache);
+            } else
+                log.info(`Will not show notification, time remaining: ${UserSettings.Instance.NewArticlesNotificationPeriod - lastNotificationBeforeMins} mins.`);
+        } else
+            log.info('Notifications disabled.');
+    }
+
+    /** Attempts to perform auto-backup according to user settings */
+    private static async TryDoAutoBackupAsync(): Promise<void> {
+        const log = Background.log.context('AutoBackup');
+        if (UserSettings.Instance.EnableAutomaticBackups) {
+            const remainingTime = Date.now() - UserSettings.Instance.LastBackupTimestamp - (UserSettings.Instance.AutomaticBackupPeriod * 60 * 60 * 1000);
+            if (remainingTime >= 0) {
+                log.info('Creating auto-backup now..');
+                const dir = JSON.parse(UserSettings.Instance.AutomaticBackupDir);
+                if ((await ScopedStorage.getPersistedUriPermissions()).indexOf(dir.url) >= 0) {
+                    log.error(`Access to ${dir.url} is not in persisted permissions list.`);
+                } else {
+                    log.debug(`Auto-backup dir: ${UserSettings.Instance.AutomaticBackupDir}`);
+                    const backupStr = await Backup.CreateBackup();
+                    try {
+                        const files = await ScopedStorage.listFiles(dir.uri);
+                        files.sort((a, b) => {
+                            if (a.name == 'NuntiBackup-latest.json')
+                                return 1;
+                            else if (b.name == 'NuntiBackup-latest.json')
+                                return -1;
+                            return a.uri < b.uri ? 1 : -1;
+                        });
+                        for (let i = 0; i < files.length; i++) {
+                            const f = files[i];
+                            if (f.name == 'NuntiBackup-latest.json')
+                                await ScopedStorage.rename(f.uri, 'NuntiBackup-1.json');
+                            else if (f.name == 'NuntiBackup-1.json')
+                                await ScopedStorage.rename(f.uri, 'NuntiBackup-2.json');
+                            else if (f.name == 'NuntiBackup-2.json')
+                                await ScopedStorage.rename(f.uri, 'NuntiBackup-3.json');
+                            else if (f.name == 'NuntiBackup-3.json')
+                                await ScopedStorage.deleteFile(f.uri);
+                        }
+
+                        log.debug('writing now');
+                        if (await ScopedStorage.writeFile(dir.uri, backupStr, 'NuntiBackup-latest.json', 'application/json', 'utf8') == null) {
+                            throw new Error('Write to NuntiBackup-latest.json failed.');
+                        }
+                    } catch (err) {
+                        log.error('AutoBackup failed!', err);
+                    }
+                }
+                UserSettings.Instance.LastBackupTimestamp = Date.now();
+                await UserSettings.Save();
+            } else
+                log.info(`Remaining time until next auto-backup: ${-(remainingTime / (60 * 60 * 1000)).toFixed(2)} hrs.`);
         }
     }
 }
